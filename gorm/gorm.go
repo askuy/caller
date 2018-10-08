bed4b761705f5d227778408dd31f6fb9d7850bd9
askuy <askuy@foxmail.com>
2018-10-08 23:16:31 +0800

    gorm init

---
+++ gorm/gorm.go
@@ -0,0 +1,89 @@
+package gorm
+
+import (
+	"github.com/godefault/caller/common"
+	"github.com/jinzhu/gorm"
+	"github.com/BurntSushi/toml"
+	"log"
+)
+
+var defaultCaller *callerStore
+
+type callerStore struct {
+	caller map[string]*GormClient
+	cfg  Cfg
+}
+
+type GormClient struct {
+	*gorm.DB
+}
+
+func New() common.Caller {
+	defaultCaller = &callerStore{
+		caller: make(map[string]*GormClient, 0),
+	}
+	return defaultCaller
+}
+
+func Caller(name string) *GormClient {
+	return defaultCaller.caller[name]
+}
+
+func (c *callerStore) InitCfg(cfg []byte) error {
+	if err := parseConfig(cfg, &c.cfg); err != nil {
+		return err
+	}
+	c.initGorm()
+	return nil
+}
+
+func (c *callerStore) Get(key string) interface{} {
+	return c.caller[key]
+}
+
+func (c *callerStore) Set(key string, val interface{}) {
+	c.caller[key] = val.(*GormClient)
+}
+
+func (c *callerStore) initGorm() {
+	for name, gormCfg := range c.cfg.Gorm {
+		db, err := newGorm(gormCfg)
+		if err != nil {
+			if gormCfg.Level == "panic" {
+				log.Panic("failed to connect mysql:" + ", error: " + err.Error())
+			} else {
+				log.Println("failed to connect mysql:" + ", error: " + err.Error())
+			}
+			continue
+		}
+		c.Set(name,db)
+	}
+}
+
+func parseConfig(cfg []byte, value interface{}) error {
+	var err error
+	if err = toml.Unmarshal(cfg, value); err != nil {
+		return err
+	}
+	return nil
+}
+
+func newGorm(cfg GormCfg) (db *gorm.DB, err error) {
+	// dsn = "username:password@tcp(addr)/stt_config?charset=utf8&parseTime=True&loc=Local&readTimeout=1s&timeout=1s&writeTimeout=1s"
+	db, err = gorm.Open(cfg.Dialect, cfg.Username+":"+cfg.Password+"@"+cfg.Network+"("+cfg.Addr+")/"+cfg.Db+
+		"?charset="+cfg.Charset+"&parseTime="+cfg.ParseTime+"&loc="+cfg.Loc+
+		"&timeout="+cfg.Timeout.Duration.String()+"&readTimeout="+cfg.ReadTimeout.Duration.String()+"&writeTimeout="+cfg.WriteTimeout.Duration.String())
+	if err != nil {
+		return
+	}
+	db.LogMode(cfg.Debug)
+	db.DB().SetMaxOpenConns(cfg.MaxOpenConns)
+	db.DB().SetMaxIdleConns(cfg.MaxIdleConns)
+	db.DB().SetConnMaxLifetime(cfg.ConnMaxLifetime.Duration)
+
+	err = db.DB().Ping()
+	if err != nil {
+		return
+	}
+	return
+}
---
+++ gorm/gorm_test.go
@@ -0,0 +1,39 @@
+package gorm
+
+import (
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+var cfgSuccess = `
+    [gorm.default]
+		debug = true
+        level = "panic"
+        network = "tcp"
+        dialect = "mysql"
+        addr = "127.0.0.1:3306"
+        username = "root"
+        password = ""
+        name = "oauth"
+        charset = "utf8"
+        parseTime = "True"
+        loc = "Local"
+        timeout = "1s"
+        readTimeout = "1s"
+        writeTimeout = "1s"
+        maxOpenConns = 30
+        maxIdleConns = 10
+        connMaxLifetime = "300s"
+`
+
+func TestParseConfig(t *testing.T) {
+	Convey("test parse config", t, func() {
+		obj := Cfg{}
+		err := parseConfig([]byte(cfgSuccess),&obj)
+		So(err, ShouldBeNil)
+		Convey("解析出 gorm defalut 不能为空", func() {
+			roomConfig := obj.Gorm["default"]
+			So(roomConfig, ShouldNotBeNil)
+		})
+	})
+}
